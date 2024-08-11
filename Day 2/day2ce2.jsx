ce8f74fa47b646a7f9f17413cbcb69c9f3cb7765
Nandhana-14 <727723eucs138@skcet.ac.in>
2024-08-11 11:26:40 +0530

    Add files via upload

---
+++ Day 2/day2ce2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Cw=()=>
+{
+    return(
+        <>
+        <h1 style ={{color:'green'}}>Hello</h1>
+        <div style={{backgroundColor:'lightblue',color:'darkblue',padding:'10px',border:'1px solid blue',borderRadius:'5px'}}>
+            <p style={{color:'darkblue',fontSize:"16px"}}>This is the inlined style paragraph</p>
+        </div>
+        </>
+    );
+};
+export default Cw;
